18e9785091b98a508828d3609ad41e78e13f5aed
Aleksis Boters 211RDB214 <125483680+aflexxxx@users.noreply.github.com>
2023-03-18 17:56:20 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,12 +2,27 @@
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(n // 2, -1, -1):
+        swaps = sift_down(i, data, swaps)
     return swaps
+
+def sift_down(i, data, swaps):
+    n = len(data)
+    min_index = i
+    l = 2 * i + 1
+    if l < n and data[l] < data[min_index]:
+        min_index = l
+    r = 2 * i + 2
+    if r < n and data[r] < data[min_index]:
+        min_index = r
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps = sift_down(min_index, data, swaps)
+    return swaps
+
 
 
 def main():
@@ -30,13 +45,19 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
+    assert len(swaps) <=4 * len(data)
 
     # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    if len(swaps) == 0:
+        print(len(swaps))
+        print("The input array is already a heap, because it is sorted in increasing order.")
+    else:
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
+
 
 
 if __name__ == "__main__":
     main()
+
